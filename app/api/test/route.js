471b559ca3644bdd26d800648af78d79825e8397
Soab42 <108150913+Soab42@users.noreply.github.com>
2023-10-11 00:47:31 +0600

    update

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -1,27 +1,5 @@
-export async function GET(request) {
-  // Create a JSON representation of the request object
-  const requestJson = {
-    method: request.method,
-    url: request.url,
+import React from "react";
 
-    headers: Object.fromEntries(request.headers),
-    ipAddress:
-      request.headers.get("x-forwarded-for") ||
-      request.headers.get("cf-connecting-ip") ||
-      request.headers.get("x-real-ip") ||
-      request.headers.get("remote-addr") ||
-      request.headers.get("x-cluster-client-ip") ||
-      request.headers.get("x-client-ip") ||
-      request.headers.get("x-forwarded") ||
-      request.headers.get("forwarded-for"),
-
-    // You can add more properties from the request object as needed
-  };
-
-  // Return the request JSON in the response
-  return new Response(JSON.stringify(requestJson), {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+export default function route() {
+  return <div>route</div>;
 }
