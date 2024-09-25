05983daa8e4eb719bf962d484e52e924fc722a79
syfuddhin <syfuddhin@gmai.com>
2024-09-25 11:29:19 +0600

    testing

--- app/message/page.js
+++ app/message/page.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 import React, { useEffect, useState } from "react";
 
@@ -51,57 +50,3 @@ export default function Page() {
 
   return <div>Loading...</div>;
 }
-=======
-"use client";
-import React, { useEffect, useState } from "react";
-
-export default function Page() {
-  const [data, setData] = useState(null); // Initialize with null
-  const [error, setError] = useState(null); // Initialize with null
-
-  useEffect(() => {
-    fetch("api/message") // Corrected route URL
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json(); // Convert the response to JSON
-      })
-      .then((jsonData) => setData(jsonData)) // Set the data in the state
-      .catch((err) => setError(err)); // Set the error in the state
-  }, []);
-
-  // console.log(data);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (data) {
-    return (
-      <div className="viewMessage">
-        <div className="layoutMessage">
-          {data &&
-            data.map((singleData, i) => {
-              return (
-                <div className="messageBox" key={i}>
-                  <div className="name">{singleData.name} </div>
-                  <div className="email">
-                    <span>from:</span>
-                    {singleData.email}
-                  </div>
-                  <div className="message">
-                    &quot; {singleData.message}.&quot;
-                  </div>
-                  <div className="time">{Date(singleData.date)}</div>
-                </div>
-              );
-            })}
-        </div>
-      </div>
-    );
-  }
-
-  return <div>Loading...</div>;
-}
->>>>>>> 41aacb10138a7bc96d479eebc451410c1dd06ab7
--- fireConfig.js
+++ fireConfig.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const fireConfig = {
   type: "service_account",
   project_id: "syfuddhin2k23",
@@ -14,20 +13,3 @@ export const fireConfig = {
     "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-qfx0j%40syfuddhin2k23.iam.gserviceaccount.com",
   universe_domain: "googleapis.com",
 };
-=======
-export const fireConfig = {
-  type: "service_account",
-  project_id: "syfuddhin2k23",
-  private_key_id: "a3995699a9c43f930f5cf780f20e350c17fc5af0",
-  private_key:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDFmWQyW+LoepUg\nMbfjnplbJbYPIQXReWHEbUBdwo92MVqbzQlDGuRSLtxccypYhNhkoUoVcoFkrkXF\nQ8N79w8nRUSCaLWC8ZwAdeFoTgg0GWoeFxX3MzSQxNmjwFEKDffvWuQUwcgS3SUC\n4ZlkKg5PZz6Qoo4p33A2rwnIo04sNbFiNIcksf6L1Ux/WFwpTQIETq+dtbHicU7w\nzPfsXaYMP+nm/K2QNFEJ/WscYJuWkb8l7TLU890opGlF/65AaY3hsq3jxVSGyc/x\nGPZ1KP7b+t8c+0PeRXbPC2juQGgvYMVN5mJFzsel+wCoRlKLSnxmz9rvqdzcVt4P\n7nXsYiYTAgMBAAECgf9tKb56E7b+bwNhWBY/F19YaTPiAzfF75IThJDmFt9z2hlZ\nHezbbttGi8F6v0mBC1DJ/gfK9gmqcOXRgsLFcEoTZ+fe/x5sXVc/fClUCKNbAGYx\n8EXpJqnjmbgQdeVz8bFQilO//04/r+x5Yd+lLU2V/gYAKqk6weyqAAlnqfTzmyl6\nHNlQt/dekCtLxhvkqf9EaFrY6QWM+6egA2wxWRXbvHxPH2CrJC1HeubQOFQQY3xn\n5r97QyBLDcYdilzqyYG1JjI22oP36DjoPDqrwb2y2IooYczTYepXqjeil+Sj8E6E\ne9uA9s9soFyNmrdil7c3QbGGwlWweMAvpuHER1ECgYEA/fMz3H4ZJL2IjDDJmgN3\nTlIn6KZnnHXnWaEra8A0Hb0PeaFxEvJnAbPJ7fRAhD+zCYJ+6AHhYghJLW6W3iU4\nmsfZdKAiSzv+muzoBmmDKfnUm0SRPEDDQCa+vHcc9v0ZydzLXmAlbPEEt8WrXMp2\naSqUEWMQL21eow02QgRi2N8CgYEAxzG82Cm1C0uIBuWszA87RBx5LKVe3s3CN3FU\nqUc2N4QBRUOrM49qjSc+Ba0TrltC0jI+sU6oZSz1vyfHG01YATQSSWoTfjyVE3pP\n+tE7lTkBobScyfRSU5mLRcfmwK0mwTXAWUC/ip4xHNvxnZgwNTK3TAK7ByBzjh5v\nar6XdU0CgYEAggrAS/Dmv0GvYtq44f1UYu7/MMvF0wEtb2Bw88SA65mdCxP461Lm\nHKEMjqW9Q/u6muwQQI5189V6Y6F18FvvGJOub2JxLUWTo5IdXus5F42vqbL7DHe9\nhGRS1//G/VOumQhPvAxQHfFuEQ1QKzwiVshO+nhYGXFQPP7PAg9s7tECgYEAusWu\nNllVAlQ+JYlxbpzRi1nPXMdqnyr68pV3NoopEgDaWCHNAzl0RQoIo7XXgGLd0s19\n7y67wrJXcndPwA/Tdr1mcCPgRzty5KKJH7rUTWT4XLFHWi2WjACqkDGR8hPpBLzr\nsER/1O/jSRYKBSTP/vGg4Mqmp1YMQ7PR8Jro1PkCgYEAyAa9XShvh1jLNmqP7pff\nGtCQob+qj1QHEwjVHD/urmTQzdOBKRKwKwXciv5DuDr6BZGryNWPgdMNXzUNFSdn\nwyVIWw3+coCNmxGSnHaGLS3Kz4KtyKSPFryxIVjBMbPvw2956orSSzz/DNSxgUDA\nPLny9RDYFqHQnqW/hG+5n94=\n-----END PRIVATE KEY-----\n",
-  client_email: "firebase-adminsdk-qfx0j@syfuddhin2k23.iam.gserviceaccount.com",
-  client_id: "107049773398753139098",
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-qfx0j%40syfuddhin2k23.iam.gserviceaccount.com",
-  universe_domain: "googleapis.com",
-};
->>>>>>> 41aacb10138a7bc96d479eebc451410c1dd06ab7
