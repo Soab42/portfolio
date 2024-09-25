4fb664ae80fa8221ed8cdeaec7c0a07a63483da4
Soab Mahmud Syfuddhin <108150913+Soab42@users.noreply.github.com>
2024-09-25 11:05:59 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+
 const nextConfig = {};
 
 module.exports = nextConfig;
-=======
-const nextConfig = {};
 
-module.exports = nextConfig;
->>>>>>> 41aacb10138a7bc96d479eebc451410c1dd06ab7
